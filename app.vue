40e29d97a88ba19a806e3cdd4ca809e1b053b29d
David Torosyan <davidtorosyan.git@gmail.com>
2024-03-24 14:47:54 -0700

    Broken composables

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import 'primevue/resources/themes/aura-light-green/theme.css'
 
 const sciencePackOptions = ref([
@@ -6,6 +6,32 @@ const sciencePackOptions = ref([
   { name: 'Green', value: 'logistic-science-pack' },
 ])
 const sciencePacks = ref([])
+
+const recipes = useRecipes()
+
+const manifest = computed(() => {
+  const queue = []
+  queue.push(...sciencePacks.value)
+
+  const result = []
+
+  // while (queue.length > 0) {
+  //   const item = queue.shift()
+
+  //   const recipe = recipes[item]
+  //   if (!recipe) {
+  //     console.error(`No recipe found for ${item}`)
+  //     result.push(recipe)
+  //     continue
+  //   }
+
+  //   for (const ingredient of recipe.ingredients) {
+  //     queue.push(ingredient.name)
+  //   }
+  // }
+
+  return result
+})
 
 </script>
 
@@ -21,5 +47,9 @@ const sciencePacks = ref([])
       aria-labelledby="multiple"
     />
     Selected: {{ sciencePacks }}
+    <!-- JSON: {{ recipes }} -->
+    <!-- {{ recipes[sciencePacks[0]]?.ingredients }} -->
+    Manifest: {{ manifest }} 
+    foo: {{ foo }}
   </div>
 </template>
---
+++ composables/useRecipes.ts
@@ -0,0 +1,62 @@
+const contentJson = await queryContent('/data-raw').findOne()
+
+type Ingredient = {
+  name: string,
+  count: number,
+}
+
+type Recipe = {
+  name: string,
+  ingredients: Ingredient[]
+}
+
+type RecipeMap = {
+  [key: string]: Recipe
+}
+
+export default function (): Ref<RecipeMap> {
+  const result = ref({} as RecipeMap)
+
+  const recipesJson = contentJson.recipe
+
+  for (const recipeJson of Object.values(recipesJson) as any[]) {
+    const nameJson = recipeJson.name
+  
+    if (typeof nameJson !== 'string') {
+      continue
+    }
+    const name = nameJson
+    const ingredients = convertIngredients(recipeJson)
+
+    result.value[name] = {
+      name,
+      ingredients
+    }
+  }
+
+  return result
+}
+
+function convertIngredients(recipeJson: any) : Ingredient[] {
+  const ingredientsJson = recipeJson.ingredients
+  const normalJson = recipeJson.normal
+  const ingredients = []
+
+  const ingredientListJson = ingredientsJson ?? normalJson.ingredients
+  for (const ingredientJson of ingredientListJson) {
+    const nameJson = ingredientJson[0]
+    if (typeof nameJson !== 'string') {
+      continue
+    }
+    const countJson = ingredientJson[1]
+
+    const name = nameJson
+    const count = countJson
+    ingredients.push({
+      name,
+      count
+    })
+  }
+
+  return ingredients
+}
