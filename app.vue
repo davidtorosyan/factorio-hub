7629db189a954f0b34189c96d1cf32fe60aac930
David Torosyan <davidtorosyan.git@gmail.com>
2024-03-24 13:03:40 -0700

    Disable vue formatter by default

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup>
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
+const color = ref('')
+
+</script>
+
 <template>
   <div>
-    <h1>Hey2</h1>
-    <NuxtWelcome />
+    <h1>Factorio Planner</h1>
+    <ColorPicker
+      v-model="color"
+      inline
+    />
+    Color: {{ color }}
   </div>
 </template>
