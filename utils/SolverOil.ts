92ebbd0641b0e739947be86200423ab598b0ecf7
David Torosyan <davidtorosyan.git@gmail.com>
2024-03-30 18:02:01 -0700

    Finish solver, untested

--- utils/SolverOil.ts
+++ utils/SolverOil.ts
@@ -22,6 +22,7 @@ type SolverOilConfig = {
 }
 
 type SolverOilOutput = {
+  crudeRate: number
   petrolRate: number
   heavyOilRate: number
   lightOilRate: number
@@ -50,14 +51,37 @@ function solveOil(input: SolverOilInput, config: SolverOilConfig): SolverOilOutp
       (yieldPetrol * config.advancedOilConfig.petrolRate)
     )
 
-  const heavyCrackingCount = 0
-  const lightCrackingCount = 0
+  const heavyCrackingCount = 
+  (
+    advancedOilCount * config.advancedOilConfig.heavyOilRate - 
+    input.heavyOilRate
+  ) / 
+  config.heavyCrackingConfig.heavyOilRate
+  
 
-  const petrolRate = 0
-  const lightOilRate = 0
-  const heavyOilRate = 0
+  const lightCrackingCount = (
+    input.petrolRate - 
+    advancedOilCount * config.advancedOilConfig.petrolRate
+  ) /
+  config.lightCrackingConfig.petrolRate
+
+  const crudeRate = advancedOilCount * config.advancedOilConfig.crudeRate
+
+  const petrolRate = 
+    advancedOilCount * config.advancedOilConfig.petrolRate +
+    lightCrackingCount * config.lightCrackingConfig.petrolRate
+
+  const lightOilRate = 
+    advancedOilCount * config.advancedOilConfig.lightOilRate +
+    heavyCrackingCount * config.heavyCrackingConfig.lightOilRate -
+    lightCrackingCount * config.lightCrackingConfig.lightOilRate
+
+  const heavyOilRate = 
+      advancedOilCount * config.advancedOilConfig.heavyOilRate -
+      heavyCrackingCount * config.heavyCrackingConfig.heavyOilRate
 
   return {
+    crudeRate,
     petrolRate,
     heavyOilRate,
     lightOilRate,
