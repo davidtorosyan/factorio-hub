f510b60818c436dc179bfea23fc1a78ea4b56b77
David Torosyan <davidtorosyan.git@gmail.com>
2024-04-18 23:51:48 -0700

    Add lab input groups

--- components/config/ConfigLab.vue
+++ components/config/ConfigLab.vue
@@ -16,17 +16,42 @@ const model = defineModel<FactoryConfig>({ required: true })
       />
     </template>
 
-    Count: <InputNumber
-      v-model="model.labs"
-      show-buttons
-    />
-    Speed: <Dropdown
-      v-model="model.speedEffect"
-      :options="data.science.speedEffects"
-    />
-    Research time: <Dropdown
-      v-model="model.researchTime"
-      :options="data.science.researchTimes"
-    />
+    <div class="lab">
+      <InputGroup>
+        <InputGroupAddon>Count</InputGroupAddon>
+        <InputNumber
+          v-model="model.labs"
+          show-buttons
+        />
+      </InputGroup>
+
+      <InputGroup>
+        <InputGroupAddon>Speed</InputGroupAddon>
+        <Dropdown
+          v-model="model.speedEffect"
+          :options="data.science.speedEffects"
+        />
+      </InputGroup>
+
+      <InputGroup>
+        <InputGroupAddon>Research time</InputGroupAddon>
+        <Dropdown
+          v-model="model.researchTime"
+          :options="data.science.researchTimes"
+        />
+      </InputGroup>
+    </div>
   </Panel>
 </template>
+
+<style scoped>
+  .lab {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .lab .p-inputgroup:not(:first-child) {
+    margin-left: 20px; 
+  }
+</style>
