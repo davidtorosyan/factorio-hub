3f12496518b611e387dadc74eb4dbd00a4024f4e
David Torosyan <davidtorosyan.git@gmail.com>
2024-05-10 23:32:52 -0700

    Add github link

---
+++ components/GithubLink.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLink
+    to="https://github.com/davidtorosyan/factorio-planner"
+    target="_blank"
+    rel="noopener"
+  >
+    <Button
+      icon="pi pi-github"
+      link
+    />
+  </NuxtLink>
+</template>
